dbbf2f9c595e69c908320c93fb883111d9ec2f65
Grommzy <grommzy@gmail.com>
2025-06-10 22:40:38 +1000

    Scaffold: CMake, FetchContent_Dependencies, Folder_Layout

---
+++ cmake/FetchSDL3.cmake
@@ -0,0 +1,13 @@
+include(FetchContent)
+
+if(NOT TARGET SDL3::SDL3)
+    FetchContent_Declare(
+        SDL3
+        GIT_REPOSITORY  https://github.com/libsdl-org/SDL.git
+        GIT_TAG         main
+        GIT_SHALLOW     TRUE
+    )
+    set(SDL_STATIC      ${RF_STATIC_SDL})
+    set(SDL_SHARED      ON)     # Both artefacts get built; we link the one we want.
+    FetchContent_MakeAvailable(SDL3)
+endif()
---
+++ cmake/Options.cmake
@@ -0,0 +1,3 @@
+option(RF_ENABLE_GL_DEBUG   "Enable OpenGL debug Callbacks in Debug builds" ON)
+option(RF_STATIC_SDL        "Link SDL3 statically instead of dynamically"   OFF)
+option(RF_LOG_LEVEL         "0 = Errors only ... 3 = Verbose"               2)
